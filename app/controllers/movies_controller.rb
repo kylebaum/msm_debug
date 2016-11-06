0f25f7f1d5a7be5a273234328acfe6100979ee81
Kyle Baum <username@users.noreply.github.com>
2016-11-06 11:02:53 -0600

    show,delete,edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,23 +4,24 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[id])
+      @movie = Movie.find_by({ :id => params[:id] })
+
   end
 
   def new_form
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    create = Movie.new
+    create.title = params[:title]
+    create.year = params[:year]
+    create.duration = params[:duration]
+    create.description = params[:description]
+    create.image_url = params[:image_url]
+    create.director_id = params[:director_id]
+    create.save
 
-    @movie.save
-
-    render("show")
+    redirect_to("show")
   end
 
   def edit_form
@@ -28,19 +29,22 @@ class MoviesController < ApplicationController
   end
 
   def update_row
+    @movie = Movie.find_by({ :id => params[:id] })
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
     @movie.director_id = params[:director_id]
+    @movie.save
 
     render("show")
   end
 
   def destroy
-    movie = Movie.find(params[:id])
+   @movie = Movie.find_by({ :id => params[:id] })
+   @movie.destroy
 
-    movie.destroy
+
   end
 end
